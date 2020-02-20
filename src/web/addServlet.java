ca1f908fccc88bd670d407597c490f0decd58d37
Liquor <2305465911@qq.com>
2020-02-20 18:13:27 +0800

    雨量监测信息 实现更新git自动显示页面 实现新增雨量信息 新增数据

---
+++ src/web/addServlet.java
@@ -0,0 +1,59 @@
+package web;
+
+import entity.RainQuality;
+import service.RainMangerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet(name = "addServlet",urlPatterns = {"/add"})
+public class addServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //解决乱码
+        request.setCharacterEncoding("utf-8");
+        //调用service层新增雨量信息的方法
+        RainMangerService rainMangerService = new RainMangerService();
+        //获取参数
+         String districtName=request.getParameter("districtName");
+         String monitorTime=request.getParameter("monitorTime");
+         int rain1 =Integer.valueOf(request.getParameter("rain"));
+         String monitoringStation=request.getParameter("monitoringStation");
+         String monitoringAddress=request.getParameter("monitoringAddress");
+         //把参数封装到对象中
+        RainQuality rain = new RainQuality();
+        //String转换为Date
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        try {
+            Date date = sdf.parse(monitorTime);
+            rain.setMonitorTime(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        rain.setMonitoringAddress(monitoringAddress);
+        rain.setMonitoringStation(monitoringStation);
+        rain.setDistrictName(districtName);
+        rain.setRain(rain1);
+
+
+        //执行添加的方法
+        int i= rainMangerService.addNewRainInfo(rain);
+        if(i==1){
+            System.out.println("ok");
+            //跳转页面
+            request.getRequestDispatcher("index").forward(request,response);
+        }else {
+            System.out.println("错误");
+            response.sendRedirect("add.jsp");
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
